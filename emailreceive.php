470ff29688664ed7ea5a283b3671775627a8e1b5
Atennapper <atenesser@gmail.com>
2015-03-21 13:26:44 -0700

    Update emailreceive.php

--- emailreceive.php
+++ emailreceive.php
@@ -1,5 +1,5 @@
 <?php
-$book = fopen("book.txt", "a") or die("Error!");
+$book = fopen("https://cdn.rawgit.com/ATENESSER/Web/master/book.txt", "a") or die("Error!");
 echo ($_POST['email']);
 fwrite($book, $email);
 fclose($book);
